98665ae0754cc0cc0c8b0024b91f201123b19943
José Ignacio Amelivia <ohcan2@gmail.com>
2019-11-27 21:09:30 +0100

    Starting to use webpack for bundling

---
+++ public/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+module.exports = {
+	entry: './src/js/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}, {
+				test: /\.(png|jpe?g|gif|svg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							outputPath: 'images'
+						}
+					}
+				]
+			}, {
+				test: /\.(woff|woff2|ttf|otf|eot)$/,
+					use: [
+						{
+							loader: "file-loader",
+							options: {
+								outputPath: 'fonts'
+							}
+						}
+					]
+			}
+		]
+	},
+	mode: 'development'
+}
